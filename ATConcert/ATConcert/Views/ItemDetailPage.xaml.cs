56234472b3de6d97b0020ed8a177348b17cf0e3d
BonAxel <123560647+BonAxel@users.noreply.github.com>
2023-12-20 13:13:06 +0100

    Add project files.

---
+++ ATConcert/ATConcert/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ATConcert.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ATConcert.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
